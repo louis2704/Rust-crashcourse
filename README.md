ec712e8d412a20ea1e97aaeffb110ee16998ad34
Sosthene00 <674694@protonmail.ch>
2022-10-19 13:12:44 +0200

    decl macros exercice

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 * We want to be able to create those geometric forms with parameters of different types (u32, f64, String...).
 * We want to implement the same operation for all those different forms
 * Some forms may be composed of other forms
+
+## Course 2: declarative macros
+
+* we have a macro `simple_vec` that takes 0 or more arguments of the same type and return a vector
+* we want to add an arm to this macro to return a vector with `n` times values `x`
---
+++ examples/decl_macro.rs
@@ -0,0 +1,27 @@
+// declarative macros looks like function
+// `println!` or `vec!` are examples or declarative macros
+
+// They're declared with the keyword `macro_rules!`
+macro_rules! simple_vec {
+    // Basically, a decl macro is a `match` statement
+    // inputs are matched against each arm
+    ( $( $x:expr ),* ) => { // Here I match 0 or more expressions, comma separated
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )* // Here the $()* indicate that this code will repeat as many times as there are inputs
+            temp_vec // We return the vector
+        }
+    };
+    // Add another arm here so that we can initialize vec3 in main()
+}
+
+fn main() {
+    let vec1 = simple_vec!('a', 'b', 'c');
+    let vec2 = simple_vec!(1, 2, 3);
+    let vec3 = simple_vec!("coucou";5);
+
+    println!("{:?}\n{:?}", vec1, vec2);
+    println!("{:?}", vec3);
+}
